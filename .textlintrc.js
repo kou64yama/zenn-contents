6d1a0aeea7fd2b6ffb16b356581b489ca73db063
Yamada Koji <kou64yama@gmail.com>
2021-09-20 21:33:25 +0900

    全角文字と半角文字の間にスペースを入れるように変更
    Prettier では全角文字と半角文字の間にスペースを入れるため，textlint で
    も同じルールになるようにします．

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   rules: {
-    "preset-ja-spacing": true,
+    "preset-ja-spacing": {
+      "ja-space-between-half-and-full-width": {
+        space: "always",
+        expectPunctuation: true,
+      },
+    },
     "preset-ja-technical-writing": {
       "ja-no-mixed-period": {
         periodMark: "．",
@@ -10,6 +15,7 @@ module.exports = {
     "preset-jtf-style": {
       "1.2.1.句点(。)と読点(、)": false,
       "1.2.2.ピリオド(.)とカンマ(,)": false,
+      "3.1.1.全角文字と半角文字の間": false,
       "4.1.3.ピリオド(.)、カンマ(,)": false,
     },
   },
